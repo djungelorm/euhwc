697fad9dd28bdb162d0bcd26ab473b56d1204064
Alex Collins <alexjcollins@gmail.com>
2013-08-19 22:12:54 +0000

    Make visibility of floating sidebar persist across browser sessions

--- themes/euhwc/sidebar-sidebar-floating.php
+++ themes/euhwc/sidebar-sidebar-floating.php
@@ -13,12 +13,19 @@ if ( is_active_sidebar( 'sidebar-floating' ) ) : ?>
         .click(function() {
           $('#sidebar-floating-visible').hide(200);
           $('#sidebar-floating-hidden').show();
+          sessionStorage.setItem('sidebar-floating', 'hidden');
         });
       $('#sidebar-floating-show')
         .click(function() {
           $('#sidebar-floating-hidden').hide();
           $('#sidebar-floating-visible').show(200);
+          sessionStorage.setItem('sidebar-floating', 'visible');
         });
+      var state = sessionStorage.getItem('sidebar-floating');
+      if (state == 'hidden') {
+        $('#sidebar-floating-hidden').show();
+        $('#sidebar-floating-visible').hide();
+      }
     });
   </script>
 
