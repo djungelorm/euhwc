309246d015a9dc812459cdbdd55a9c075f88d481
Alex Collins <alexjcollins@gmail.com>
2013-08-21 16:18:00 +0100

    Clean up comments

--- themes/euhwc/footer.php
+++ themes/euhwc/footer.php
@@ -4,10 +4,6 @@
  *
  * Contains footer content and the closing of the
  * #main and #page div elements.
- *
- * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
  */
 ?>
 
