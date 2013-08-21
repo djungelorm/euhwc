d13941a8daa4c40044abbcbcd10f0df8792bf186
Alex Collins <alexjcollins@gmail.com>
2013-08-21 16:47:48 +0100

    Add license and author uri

--- plugins/user-meta-widget/user-meta-widget.php
+++ plugins/user-meta-widget/user-meta-widget.php
@@ -4,6 +4,14 @@ Plugin Name: User Meta Widget
 Description: A widget that provides log out and edit profile links.
 Version: 1.0
 Author: Alex Collins
+Author URI: http://www.linkedin.com/in/alexanderjamescollins
+License: WTFPL
+*/
+/*
+Copyright © 2013 Alex Collins
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
 */
 
 class user_meta_widget extends WP_Widget {
