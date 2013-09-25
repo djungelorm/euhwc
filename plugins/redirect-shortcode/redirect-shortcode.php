66b794e56f883ef1bf3188babd210953f12e73d5
Alex Collins <alexjcollins@gmail.com>
2013-09-25 19:46:59 +0100

    Fix bug with headers already being sent

--- plugins/redirect-shortcode/redirect-shortcode.php
+++ plugins/redirect-shortcode/redirect-shortcode.php
@@ -14,13 +14,28 @@ terms of the Do What The Fuck You Want To Public License, Version 2,
 as published by Sam Hocevar. See the COPYING file for more details.
 */
 
-add_shortcode('redirect', 'do_redirect');
+add_action('template_redirect', 'redirect_shortcode_preprocess', 1);
+add_shortcode('redirect', 'redirect_shortcode_do_redirect');
 
-function do_redirect($atts) {
+function redirect_shortcode_preprocess() {
+  if (!is_singular())
+    return;
+  global $post;
+  if (!empty($post->post_content) && strpos($post->post_content, '[redirect') !== false) {
+    if (preg_match("/\[redirect url='[^']+'\]/", $post->post_content))
+      do_shortcode($post->post_content);
+  }
+}
+
+function redirect_shortcode_do_redirect($atts) {
   if (isset($atts['url']) && !empty($atts['url'])) {
     $url = esc_url($atts['url']);
+    if (headers_sent())
+      return 'Failed to redirect to <a href="'.$url.'">'.$url.'</a>';
     wp_redirect($url);
   } else {
+    if (headers_sent())
+      return 'Failed to redirect';
     wp_redirect('/404.php');
   }
   exit;
