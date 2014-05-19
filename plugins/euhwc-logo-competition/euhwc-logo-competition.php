9a3c97ce52ff2fd33442923e16c330b9a4f719b8
Alex Collins <a.collins@ed.ac.uk>
2014-05-19 15:02:55 +0100

    Add copyright

--- plugins/euhwc-logo-competition/euhwc-logo-competition.php
+++ plugins/euhwc-logo-competition/euhwc-logo-competition.php
@@ -7,6 +7,12 @@ Author: Alex Collins
 Author URI: http://www.linkedin.com/in/alexanderjamescollins
 License: WTFPL
 */
+/*
+Copyright © 2013 Alex Collins
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+*/
 
 add_action('init', 'euhwc_logo_competition_init');
 
