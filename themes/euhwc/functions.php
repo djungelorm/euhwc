b7a21a510d30df22a179d3d76fdff9b3764dec6f
Alex Collins <a.collins@ed.ac.uk>
2014-08-28 14:55:44 +0100

    Remove unnecessary tags from <head>

--- themes/euhwc/functions.php
+++ themes/euhwc/functions.php
@@ -32,6 +32,17 @@ remove_action('wp_head', 'feed_links_extra', 3);
 add_action('wp_head', 'euhwc_feed_links', 2);
 
 /**
+ * Remove crap from <head>
+ */
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+
+/**
  * Remove twentythirteen widget areas
  */
 function euhwc_widgets_remove() {
