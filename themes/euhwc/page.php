1c71beb7106f923d793bd7f891651bd1ed684c2c
Alex Collins <a.collins@ed.ac.uk>
2014-08-28 18:08:11 +0100

    Make post thumbnail link to larger image (using thickbox popup)

--- themes/euhwc/page.php
+++ themes/euhwc/page.php
@@ -18,11 +18,16 @@ get_header(); ?>
 
           <div class="entry-content">
 
-            <?php if ( has_post_thumbnail() && ! post_password_required() && get_post_meta(get_the_ID(), 'HideFeaturedImage', true) == '') : ?>
-            <div class="entry-thumbnail">
-              <?php the_post_thumbnail('medium'); ?>
-            </div>
-            <?php endif; ?>
+            <?php
+            if ( has_post_thumbnail() && ! post_password_required() && get_post_meta(get_the_ID(), 'HideFeaturedImage', true) == '') {
+              echo '<div class="entry-thumbnail">';
+              $large_image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
+              echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '" class="thickbox">';
+              the_post_thumbnail('medium');
+echo '</a>';
+              echo '</div>';
+            }
+            ?>
 
             <?php the_content(); ?>
             <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
