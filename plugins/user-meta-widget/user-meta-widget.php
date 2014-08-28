800051f0ef3051db1bd8e7ea29e01d1007cbf8c3
Alex Collins <a.collins@ed.ac.uk>
2014-08-28 15:53:23 +0100

    version 1.4

--- plugins/user-meta-widget/user-meta-widget.php
+++ plugins/user-meta-widget/user-meta-widget.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: User Meta Widget
 Description: A widget that provides links to log out and user account information.
-Version: 1.3
+Version: 1.4
 Author: Alex Collins
 Author URI: http://www.linkedin.com/in/alexanderjamescollins
 License: WTFPL
