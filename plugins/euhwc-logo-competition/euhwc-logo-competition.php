43e365d31b615c753d7111c366a2a767b50d59fa
Alex Collins <alexjcollins@gmail.com>
2015-04-20 10:44:03 +0100

    version 1.7

--- plugins/euhwc-logo-competition/euhwc-logo-competition.php
+++ plugins/euhwc-logo-competition/euhwc-logo-competition.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: EUHWC Logo Competition
 Description: Provides shortcodes to run a logo competition.
-Version: 1.6
+Version: 1.7
 Author: Alex Collins
 Author URI: http://www.linkedin.com/in/alexanderjamescollins
 License: WTFPL
